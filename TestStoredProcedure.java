dc2f683fc008983fdbc3193eac47eb1d019cf83a
tjosephcarroll <tjosephcarroll@gmail.com>
2017-05-16 18:39:52 -0400

    Initial Commit Of SP Test

---
+++ TestStoredProcedure.java
@@ -0,0 +1,29 @@
+/*
+ * Testing code to show other ways we might call a stored procedure.
+ * If we're encountering a problem with the CallableStatement code
+ * then we need to know our options with particular server versions
+ * 
+ * 
+ */
+
+import java.io.*;
+
+import java.sql.*;
+
+public class TestStoredProcedure {
+
+	public static void main(String[] args){
+		try{
+			
+			
+		}
+		catch (Exception ex) {
+ 			System.out.println("Exception: " +
+                               ex.getClass().getName()
+                               + ": " + ex.getMessage());
+ 		}
+		}
+		
+	}
+	
+}
